e1112f0e1f33e09e082fdde408b6d49e7b35416d
lapinp <nvzcsoft@yandex.ru>
2022-06-20 15:59:32 +0300

    feat(app/autorestake): send signType

--- src/views/Extensions/index.vue
+++ src/views/Extensions/index.vue
@@ -688,6 +688,9 @@ export default {
         signerWallet.value.type === WALLET_TYPES.KEPLR
       ) {
         let keplrResult;
+        const signType = extensionTransactionForSign.value.transaction.direct
+          ? 'direct'
+          : 'json';
 
         try {
           keplrResult = await keplrConnector.value.sendKeplrTransaction(
@@ -715,11 +718,13 @@ export default {
 
         const defaultTx = {
           ...keplrResult.signedTx,
+          signType,
           publicKey: signerWallet.value.getPublicKeyDecoded(),
           signature: keplrResult.signature,
         };
         const defaultSendTx = extensionTransactionForSign.value.transaction;
         const protobufTx = {
+          signType,
           mode: 'sync',
           tx: {
             memo: defaultSendTx.json.memo || '',
@@ -735,6 +740,8 @@ export default {
             ],
           },
         };
+
+        // https://core-fix-cosmos-grant.3ahtim54r.ru/api
 
         const data = await useApi('wallet').sendSignedTransaction({
           hash: keplrNetworksProtobufFormat.includes(signerWallet.value.net)
