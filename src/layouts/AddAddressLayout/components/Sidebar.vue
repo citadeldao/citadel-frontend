1247d0dda2f5097efcee54b07845cbbb95f7741d
Lustach <vladislavlusta@citadel.one>
2022-06-23 12:15:16 +0300

    fix(sortByAlphabet): sorting by code

--- src/layouts/AddAddressLayout/components/Sidebar.vue
+++ src/layouts/AddAddressLayout/components/Sidebar.vue
@@ -221,7 +221,7 @@ export default {
     });
     const filteredWallets = computed(() => {
       const data = walletsList.value;
-      const byAlphabet = sortByAlphabet(data, 'name', 'address');
+      const byAlphabet = sortByAlphabet(data, 'code', 'address');
       const byValue = data.sort((a, b) => a.balanceUSD - b.balanceUSD);
 
       switch (filterValue.value) {
