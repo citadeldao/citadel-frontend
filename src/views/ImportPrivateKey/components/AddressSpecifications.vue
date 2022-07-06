6eec3fc47c81bd0fe79b3be2342b5d9bbe5532ad
Lustach <vladislavlusta@citadel.one>
2022-07-06 13:16:49 +0300

    fix(lint)

--- src/views/ImportPrivateKey/components/AddressSpecifications.vue
+++ src/views/ImportPrivateKey/components/AddressSpecifications.vue
@@ -37,10 +37,7 @@
           data-qa="add-address__existing__private-key__key-field"
         />
       </div>
-      <PrimaryButton
-        :disabled="disabled"
-        data-qa="Import"
-      >
+      <PrimaryButton :disabled="disabled" data-qa="Import">
         {{ $t('import') }}
       </PrimaryButton>
     </form>
--- src/views/Wallet/components/CurrentAsset/SelectAssetModal/AssetItem.vue
+++ src/views/Wallet/components/CurrentAsset/SelectAssetModal/AssetItem.vue
@@ -185,9 +185,6 @@ export default {
         &__amount {
           color: $darkgray;
         }
-
-        &__currency {
-        }
       }
     }
   }
