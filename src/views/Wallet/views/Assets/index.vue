d5d592c50130440223fffd1337d297aa9912bb47
Lustach <vladislavlusta@citadel.one>
2022-07-06 16:26:20 +0300

    test for gitlab

--- src/views/Wallet/views/Assets/index.vue
+++ src/views/Wallet/views/Assets/index.vue
@@ -138,6 +138,7 @@ export default {
   },
   emits: ['prepareClaim', 'prepareXctClaim'],
   setup(props) {
+    console.log('test');
     const store = useStore();
     const route = useRoute();
     const { currentWallet: stateCurrentWallet } = useWallets();
