1732e120855e7a74f2bede845bb92b3692dbdfd9
Lustach <vladislavlusta@citadel.one>
2022-07-06 16:37:52 +0300

    Revert "fix(lint)"
    This reverts commit f644b77a236bab246effd509b42b76ec61ae5650.

--- src/utils/socketEventHandler.js
+++ src/utils/socketEventHandler.js
@@ -3,7 +3,7 @@ import useWallets from '@/compositions/useWallets';
 import { WALLET_TYPES } from '@/config/walletType';
 import notify from '@/plugins/notify';
 
-export async function socketEventHandler({ eventName, data }) {
+export async function socketEventHandler({ eventName, data }){
   // this.socket.on('transaction-events-client', (tx) => {
   //   store.commit('extensions/SET_TRANSACTION_FOR_SIGN', tx, { root: true });
   // });
@@ -13,50 +13,50 @@ export async function socketEventHandler({ eventName, data }) {
       store.commit('extensions/SET_TRANSACTION_FOR_SIGN', data, { root: true });
 
       break;
-    // this.socket.on('message-from-app', async (data) => {
-    //   if (data.type === 'view-scrt-balance') {
-    //     const secretAddress = data.message.address;
-    //     const sSecretContract = data.message.tokenContract;
+      // this.socket.on('message-from-app', async (data) => {
+      //   if (data.type === 'view-scrt-balance') {
+      //     const secretAddress = data.message.address;
+      //     const sSecretContract = data.message.tokenContract;
 
-    //     const sendErrorMsg = () => {
-    //       store.dispatch('extensions/sendCustomMsg', {
-    //         token: store.getters['extensions/currentAppInfo'].token,
-    //         message: {
-    //           balance: 'Viewingkey not found, balance: ?',
-    //           tokenContract: sSecretContract,
-    //         },
-    //         type: data.type,
-    //       });
-    //     };
+      //     const sendErrorMsg = () => {
+      //       store.dispatch('extensions/sendCustomMsg', {
+      //         token: store.getters['extensions/currentAppInfo'].token,
+      //         message: {
+      //           balance: 'Viewingkey not found, balance: ?',
+      //           tokenContract: sSecretContract,
+      //         },
+      //         type: data.type,
+      //       });
+      //     };
 
-    //     const { wallets } = useWallets();
-    //     const wallet = wallets.value
-    //       .filter(w => w.type !== WALLET_TYPES.WALLET_TYPES)
-    //       .find(w => w.address === secretAddress);
+      //     const { wallets } = useWallets();
+      //     const wallet = wallets.value
+      //       .filter(w => w.type !== WALLET_TYPES.WALLET_TYPES)
+      //       .find(w => w.address === secretAddress);
 
-    //     if (wallet) {
-    //       store.dispatch('wallets/setCurrentWallet', wallet);
-    //       const searchToken = store.getters['subtokens/formatedSubtokens']('myTokens', wallet);
+      //     if (wallet) {
+      //       store.dispatch('wallets/setCurrentWallet', wallet);
+      //       const searchToken = store.getters['subtokens/formatedSubtokens']('myTokens', wallet);
 
-    //       if (searchToken[0] && searchToken[0].tokenBalance) {
-    //         store.dispatch('extensions/sendCustomMsg', {
-    //           token: store.getters['extensions/currentAppInfo'].token,
-    //           message: {
-    //             balance: searchToken[0].tokenBalance.calculatedBalance,
-    //             tokenContract: sSecretContract,
-    //           },
-    //           type: data.type,
-    //         });
-    //       } else {
-    //         sendErrorMsg();
-    //       }
-    //     }
-    //   }
+      //       if (searchToken[0] && searchToken[0].tokenBalance) {
+      //         store.dispatch('extensions/sendCustomMsg', {
+      //           token: store.getters['extensions/currentAppInfo'].token,
+      //           message: {
+      //             balance: searchToken[0].tokenBalance.calculatedBalance,
+      //             tokenContract: sSecretContract,
+      //           },
+      //           type: data.type,
+      //         });
+      //       } else {
+      //         sendErrorMsg();
+      //       }
+      //     }
+      //   }
 
-    //   if (data.type === 'sign-message') {
-    //     store.commit('extensions/SET_MESSAGE_FOR_SIGN', data.message, { root: true });
-    //   }
-    // });
+      //   if (data.type === 'sign-message') {
+      //     store.commit('extensions/SET_MESSAGE_FOR_SIGN', data.message, { root: true });
+      //   }
+      // });
     case 'message-from-app':
       if (data.type === 'view-scrt-balance') {
         const secretAddress = data.message.address;
@@ -75,15 +75,12 @@ export async function socketEventHandler({ eventName, data }) {
 
         const { wallets } = useWallets();
         const wallet = wallets.value
-          .filter((w) => w.type !== WALLET_TYPES.WALLET_TYPES)
-          .find((w) => w.address === secretAddress);
+          .filter(w => w.type !== WALLET_TYPES.WALLET_TYPES)
+          .find(w => w.address === secretAddress);
 
         if (wallet) {
           store.dispatch('wallets/setCurrentWallet', wallet);
-          const searchToken = store.getters['subtokens/formatedSubtokens'](
-            'myTokens',
-            wallet
-          );
+          const searchToken = store.getters['subtokens/formatedSubtokens']('myTokens', wallet);
 
           if (searchToken[0] && searchToken[0].tokenBalance) {
             store.dispatch('extensions/sendCustomMsg', {
@@ -101,9 +98,7 @@ export async function socketEventHandler({ eventName, data }) {
       }
 
       if (data.type === 'sign-message') {
-        store.commit('extensions/SET_MESSAGE_FOR_SIGN', data.message, {
-          root: true,
-        });
+        store.commit('extensions/SET_MESSAGE_FOR_SIGN', data.message, { root: true });
       }
 
       break;
@@ -112,41 +107,37 @@ export async function socketEventHandler({ eventName, data }) {
       store.commit('transactions/ADD_TO_MEMPOOL', data);
 
       break;
-    // this.socket.on('mempool-remove-tx-client', async (tx) => {
-    //   // show notify when tx success
-    //   const { wallets } = useWallets();
-    //   const wallet = wallets.value.find(w => w.net === tx.net && tx.from.toLowerCase() === w.address.toLowerCase());
+      // this.socket.on('mempool-remove-tx-client', async (tx) => {
+      //   // show notify when tx success
+      //   const { wallets } = useWallets();
+      //   const wallet = wallets.value.find(w => w.net === tx.net && tx.from.toLowerCase() === w.address.toLowerCase());
 
-    //   if (wallet) {
-    //     notify({
-    //       type: tx.status === 'success' ? tx.status : 'warning',
-    //       text: '',
-    //       hash: wallet.getTxUrl(wallet.id, tx.hash),
-    //       duration: 10000,
-    //     });
-    //   }
+      //   if (wallet) {
+      //     notify({
+      //       type: tx.status === 'success' ? tx.status : 'warning',
+      //       text: '',
+      //       hash: wallet.getTxUrl(wallet.id, tx.hash),
+      //       duration: 10000,
+      //     });
+      //   }
 
-    //   const res = await citadel.parseSocketObject('mempool-remove-tx-client', tx);
-    //   if(!res.error){
-    //     store.commit('transactions/REMOVE_FROM_MEMPOOL', tx);
-    //     // store.dispatch('wallets/getNewWallets','lazy');
-    //     if(res.data.updateStakeListRequired){
-    //       await store.dispatch('staking/updateStakeList', ({ address: tx.from, net: tx.net }),{ root: true });
-    //       if(tx.from.toLowerCase() !== tx.to.toLowerCase()){
-    //         await store.dispatch('staking/updateStakeList', ({ address: tx.to, net: tx.net }),{ root: true });
-    //       }
-    //     }
-    //   }
-    // });
+      //   const res = await citadel.parseSocketObject('mempool-remove-tx-client', tx);
+      //   if(!res.error){
+      //     store.commit('transactions/REMOVE_FROM_MEMPOOL', tx);
+      //     // store.dispatch('wallets/getNewWallets','lazy');
+      //     if(res.data.updateStakeListRequired){
+      //       await store.dispatch('staking/updateStakeList', ({ address: tx.from, net: tx.net }),{ root: true });
+      //       if(tx.from.toLowerCase() !== tx.to.toLowerCase()){
+      //         await store.dispatch('staking/updateStakeList', ({ address: tx.to, net: tx.net }),{ root: true });
+      //       }
+      //     }
+      //   }
+      // });
     case 'mempool-remove-tx-client':
       // show notify when tx success
       {
         const { wallets } = useWallets();
-        const wallet = wallets.value.find(
-          (w) =>
-            w.net === data.net &&
-            data.from.toLowerCase() === w.address.toLowerCase()
-        );
+        const wallet = wallets.value.find(w => w.net === data.net && data.from.toLowerCase() === w.address.toLowerCase());
 
         if (wallet) {
           notify({
@@ -161,20 +152,12 @@ export async function socketEventHandler({ eventName, data }) {
         // if(!res.error){
         store.commit('transactions/REMOVE_FROM_MEMPOOL', data);
         // store.dispatch('wallets/getNewWallets','lazy');
-        if (data.updateStakeListRequired) {
-          await store.dispatch(
-            'staking/updateStakeList',
-            { address: data.from, net: data.net },
-            { root: true }
-          );
-          if (data.from.toLowerCase() !== data.to.toLowerCase()) {
-            await store.dispatch(
-              'staking/updateStakeList',
-              { address: data.to, net: data.net },
-              { root: true }
-            );
+        if(data.updateStakeListRequired){
+          await store.dispatch('staking/updateStakeList', ({ address: data.from, net: data.net }),{ root: true });
+          if(data.from.toLowerCase() !== data.to.toLowerCase()){
+            await store.dispatch('staking/updateStakeList', ({ address: data.to, net: data.net }),{ root: true });
           }
-          // }
+        // }
         }
       }
 
