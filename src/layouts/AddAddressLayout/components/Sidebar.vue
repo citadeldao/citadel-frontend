d3693c786a4e443d501b2219117c5f3c6e758380
yedil_a <akhmetkarimov.yedil@citadel.one>
2022-07-26 14:19:45 +0600

    Resolved issue with button height

--- src/layouts/AddAddressLayout/components/Sidebar.vue
+++ src/layouts/AddAddressLayout/components/Sidebar.vue
@@ -723,7 +723,7 @@ export default {
     align-items: center;
     justify-content: space-around;
     max-width: calc(#{$sidebar-max-width} - 50px);
-    max-height: 80px;
+    min-height: 80px;
     width: 100%;
     height: 100%;
     border-radius: 16px;
