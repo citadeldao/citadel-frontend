ca6dcbf53f2eddd7c6f5b416e2ca9bd6bb105a03
Sergey Romanov <serega_rom@bk.ru>
2022-08-04 23:27:04 +0400

    fix warning

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ import citadel from '@citadeldao/lib-citadel';
 import hljsVuePlugin from '@highlightjs/vue-plugin';
 import * as Sentry from '@sentry/vue';
 import { Integrations } from '@sentry/tracing';
-import Vue from 'vue';
+import * as Vue from 'vue';
 
 const app = createApp(App);
 
