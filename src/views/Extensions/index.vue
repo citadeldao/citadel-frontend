1730f09c64aaa71ac68f3f27bf04746535dbc4ad
Sergey Romanov <serega_rom@bk.ru>
2022-09-06 11:36:50 +0400

    remove comments

--- src/views/Extensions/index.vue
+++ src/views/Extensions/index.vue
@@ -1020,7 +1020,6 @@ export default {
             extensionsSocketTypes.types.execute
         ) {
           showConfirmModalLoading.value = true;
-
           // execute contract
           const response = await citadel.executeContract(
             signerWallet.value.id,
@@ -1033,8 +1032,6 @@ export default {
               ...extensionTransactionForSign.value.messageScrt,
             }
           );
-
-          // execute contract with message collection
           // send success message to app
           if (response?.data) {
             confirmModalDisabled.value = false;
