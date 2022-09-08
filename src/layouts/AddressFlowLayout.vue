5011ec7b8720df112a1a15d66fd7a47879ba349d
Lustach <vladislavlusta@citadel.one>
2022-09-08 14:04:05 +0300

    fix: justify-content address-layout

--- src/layouts/AddressFlowLayout.vue
+++ src/layouts/AddressFlowLayout.vue
@@ -57,7 +57,7 @@ export default {
   display: flex;
   align-items: center;
   flex-direction: column;
-  justify-content: flex-start;
+  justify-content: flex-end;
   position: relative;
   padding: 50px 0 0;
   box-sizing: border-box;
