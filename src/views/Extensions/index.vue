f5af24cabad4d9cdc48b0aa70c962fa19a532267
lapinp <nvzcsoft@yandex.ru>
2022-09-09 12:02:36 +0300

    fix(appids)

--- src/views/Extensions/index.vue
+++ src/views/Extensions/index.vue
@@ -349,9 +349,7 @@ export default {
     const showLedgerConnect = ref(false);
     const ledgerError = ref('');
     const msgSuccessSignature = ref('');
-    const fullScreenAppIds = ref([
-      6, 7, 9, 10, 12, 13, 14, 15, 16, 17, 18, 21, 22, 23,
-    ]);
+    const fullScreenAppIds = ref([6, 7, 9, 10, 12, 13, 14, 15, 21]);
 
     const { wallets: walletsList } = useWallets();
 
