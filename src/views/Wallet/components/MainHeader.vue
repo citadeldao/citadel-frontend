a40f6dd414134231f329780171d5519e13bc81b5
lapinp <nvzcsoft@yandex.ru>
2022-09-27 10:11:43 +0300

    fix(transactions): btc in old version

--- src/views/Wallet/components/MainHeader.vue
+++ src/views/Wallet/components/MainHeader.vue
@@ -51,7 +51,7 @@
         active-class="main-header__active"
         class="main-header__tabs-item"
         :to="{
-          name: !['polkadot'].includes($route.params.net)
+          name: !['btc', 'polkadot'].includes($route.params.net)
             ? 'WalletTransactions'
             : 'WalletTransactionsOld',
           params: { ...$route.params },
