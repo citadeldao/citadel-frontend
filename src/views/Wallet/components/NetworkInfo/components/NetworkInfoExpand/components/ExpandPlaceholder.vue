3d9e06c8b21f9eb0756ac9cb41fb967668f85d76
unixhelloworld <akhmetkarimov.yedil@gmail.com>
2022-11-03 02:55:31 +0600

    Added default output for netInfo; Fixed constrict and expand icons

---
+++ src/views/Wallet/components/NetworkInfo/components/NetworkInfoExpand/components/ExpandPlaceholder.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="expand__placeholder">
+    <Placeholder />
+    <p class="title-text">{{ $t('netInfoGeneral.placeholderTitle') }}</p>
+    <p class="description-text color-mid-blue">
+      {{ $t('netInfoGeneral.placeholderDescription') }}
+    </p>
+  </div>
+</template>
+
+<script>
+import Placeholder from '@/assets/icons/network-info/expand-placeholder.svg';
+export default {
+  name: 'ExpandInfoPlaceholder',
+  components: {
+    Placeholder,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.expand__placeholder {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+
+  .title-text,
+  .description-text {
+    margin: 0;
+  }
+
+  .title-text {
+    margin: 30px 0 15px;
+  }
+}
+</style>
