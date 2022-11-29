3d5b40a375d1bd5933789b7ace74b2dc449c57aa
lapinp <nvzcsoft@yandex.ru>
2022-11-29 15:05:42 +0300

    CW-2203: fix(unselect): fix networks

--- src/compositions/useCheckItem.js
+++ src/compositions/useCheckItem.js
@@ -11,7 +11,9 @@ export default function useCheckItem() {
   };
   const removeItem = (id, remove) => {
     if (remove) return;
-    checkedItems.value = checkedItems.value.filter((item) => item !== id);
+    checkedItems.value = checkedItems.value.filter(
+      (item) => item !== (typeof id === 'number' ? id : id.id)
+    );
   };
 
   const addSingleItem = (id) => {
