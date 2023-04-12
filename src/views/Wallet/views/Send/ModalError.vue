973512707eab7650826c182d0763ff7661a11af0
lapinp <nvzcsoft@yandex.ru>
2023-04-12 17:37:28 +0300

    refact(send modals)

---
+++ src/views/Wallet/views/Send/ModalError.vue
@@ -0,0 +1,33 @@
+<template>
+  <ModalContent
+    v-click-away="errorCloseHandler"
+    title="Warning"
+    icon="warningIcon"
+    :desc="txError"
+    button-text="ok"
+    type="warning"
+    @close="errorCloseHandler"
+    @buttonClick="errorClickHandler"
+  />
+</template>
+<script>
+import ModalContent from '@/components/ModalContent';
+
+export default {
+  name: 'ModalErrorSend',
+  components: {
+    ModalContent,
+  },
+  props: {
+    txError: {
+      required: true,
+    },
+    errorCloseHandler: {
+      required: true,
+    },
+    errorClickHandler: {
+      required: true,
+    },
+  },
+};
+</script>
