23c35925d6fac79a5ba69c42665767c5559c7404
lapinp <nvzcsoft@yandex.ru>
2023-07-18 15:38:38 +0300

    CW-2822: feat - hide balance optional

--- src/store/modules/balance.js
+++ src/store/modules/balance.js
@@ -1,5 +1,6 @@
 const types = {
   SET_BALANCE: 'SET_BALANCE',
+  TOGGLE_BALANCE: 'TOGGLE_BALANCE',
 };
 
 export default {
@@ -8,21 +9,29 @@ export default {
     usd: 0,
     usdWithTokens: 0,
     btc: 0,
+    showBalance: true,
   }),
 
   getters: {
+    showBalance: (state) => state.showBalance,
     usd: (state) => state.usd,
     usdWithTokens: (state) => state.usdWithTokens,
     btc: (state) => state.btc,
   },
 
   mutations: {
+    [types.TOGGLE_BALANCE](state, value) {
+      state.showBalance = value;
+    },
     [types.SET_BALANCE](state, { type, value }) {
       state[type] = value;
     },
   },
 
   actions: {
+    toggleShowBalance({ commit }, value) {
+      commit(types.TOGGLE_BALANCE, value);
+    },
     setBalance({ commit }, { type, value }) {
       commit(types.SET_BALANCE, { type, value });
     },
