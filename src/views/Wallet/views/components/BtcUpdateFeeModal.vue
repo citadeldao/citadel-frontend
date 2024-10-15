64f817df2c68a55c008bbb8036e92662a54e0382
papabuylow <nvzcsoft@yandex.ru>
2024-10-15 17:02:22 +0300

    fix: add link explorer to notify

--- src/views/Wallet/views/components/BtcUpdateFeeModal.vue
+++ src/views/Wallet/views/components/BtcUpdateFeeModal.vue
@@ -10,7 +10,7 @@
     @buttonClick="confirmClickHandler"
   >
     <teleport to="body">
-      <Modal v-if="showSuccessModal">
+      <!-- <Modal v-if="showSuccessModal">
         <SuccessModal
           :close-success-modal="closeSuccessModal"
           :success-click-handler="successClickHandler"
@@ -18,7 +18,7 @@
           :success-tx="successTx"
           @changeComment="onChangeComment"
         />
-      </Modal>
+      </Modal> -->
       <Modal v-if="showLedgerConnect">
         <ConfirmLedgerModal
           v-if="showLedgerConnect"
@@ -184,6 +184,7 @@ export default {
     };
 
     const onChangeFeeSend = async (feeData) => {
+      console.log('FEE DATA', feeData);
       feeType.value = feeData;
       const fee = dataFee.value[feeType.value]?.fee;
 
@@ -252,6 +253,7 @@ export default {
         notify({
           type: 'success',
           text: 'Transaction fee has been successfully increased',
+          hash: `https://blockchair.com/bitcoin/transaction/${result.data[0]}`,
         });
         emit('close');
       } else {
@@ -304,6 +306,9 @@ export default {
         toAddress.value = props.signerWallet.address;
       }
       amount.value = getBalance(props.txInfo);
+      setTimeout(() => {
+        onChangeFeeSend('medium');
+      }, 1000);
     });
 
     return {
