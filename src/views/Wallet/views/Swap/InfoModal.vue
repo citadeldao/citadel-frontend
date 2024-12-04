06217f5f6248818c3bb78c3976705011d43a7cdf
Пётр Лапин <plapinp@MacBook-Pro-Petr.local>
2024-12-04 10:23:22 +0300

    fix: log wasm msg

--- src/views/Wallet/views/Swap/InfoModal.vue
+++ src/views/Wallet/views/Swap/InfoModal.vue
@@ -234,6 +234,7 @@ export default {
         isCosmosTx.value = true;
         const msgObj = JSON.parse(props.txRoute.transactionRequest.data);
         let wasmStructure;
+        console.log('msg log', msgObj);
         if (msgObj.msg.wasm) {
           wasmStructure = { ...msgObj.msg.wasm };
           wasmStructure.sender = props.signerWallet.address;
