3302979393b8024e889749398c3b5daf2d4d4bd8
plapinp plapinp <plapinp17@gmail.com>
2025-03-11 17:20:43 +0300

    fix: tx check components fix

--- src/views/Wallet/views/Transactions/components/InfoBlock.vue
+++ src/views/Wallet/views/Transactions/components/InfoBlock.vue
@@ -112,7 +112,7 @@ export default {
       let findType = 0;
       props.info.view.forEach((v) => {
         v.components.forEach((w) => {
-          if (w.type === 'amount') {
+          if (w.type === 'amount' && !findType) {
             findType = w;
           }
         });
